604ab5d27cb00dd9e6a94bcf71c8bbe9d534538c
Michele Ferrero <ferrerom@eurecom.fr>
2024-01-03 09:15:55 +0100

    update clouds

--- CLOUDS/code_lab_2/Map_Reduce/reduce/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import Tuple
-
-def main(input) -> Tuple[str,int]:
-    #converting from list of obj to list of int
-    for i in input[1]:
-        i=int(i)
-    return (input[0],sum(input[1]))
